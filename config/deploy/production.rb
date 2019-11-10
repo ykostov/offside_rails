ce8ad88e7f29222893e6c1612323865671434f69
Rumen Milushev <r@vert.bg>
2019-11-10 14:44:06 +0200

    Add server name and deployer

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :stage, :production
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
-set :server_name, 'burgas-reporter.com'
-server 'vert', user: 'deployer', roles: %w( web app db ), primary: true
+set :server_name, 'offside24.net'
+server 'vert', user: 'offside24', roles: %w( web app db ), primary: true
 set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
 set :rails_env, :production
 set :unocorn_worker_count, 5
