f57086db574ac82302286aa27a1e7ece6ddde7cd
Yasen Kostov <qskatak04@gmail.com>
2020-03-31 17:23:47 +0300

    Add files via upload

---
+++ public/ultrabg/contact.php
@@ -0,0 +1,29 @@
+<?php
+
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+
+  $email_from = 'hoteldelmarbg@gmail.com';
+
+  $email_subject = "New Form Send";
+
+  $email_body = "User Name: $name.\n".
+                  "User Email: $visitor_email.\n".
+                    "User Message= $message.\n";
+
+  $to = "qskatak04@gmail.com";
+
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+
+  header("Location: contact us.html");
+
+
+
+
+?>
